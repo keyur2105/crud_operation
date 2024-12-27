8f541c4a84935651f6aafbab6235f23179f327f9
shivangi <shivangi@gmail.com>
2024-12-27 14:24:49 +0530

    finish crud

--- data/tempCodeRunnerFile.py
+++ data/tempCodeRunnerFile.py
@@ -1,2 +1,12 @@
- # newd = [x for x in data if str(x['id']) != str(id)]
-    # print('new data :',newd)
+    
+# def updateprocess(request):
+#     id = request.GET.get('update')
+#     u_name = request.GET.get("name")
+#     u_password = request.GET.get('password')
+#     print(u_name, u_password)
+
+#     global data
+#     for i in data:
+#         if str(i['id']) == str(id):
+#             i['name'] = u_name
+#             i['password'] = u_password
--- data/views.py
+++ data/views.py
@@ -1,58 +1,43 @@
 from django.shortcuts import render, HttpResponse, redirect
 
-
-def data_input(request):
-    name = request.GET.get('name')
-    password = request.GET.get('pw')
-    # print("1",name, password)
-    
-    if name and password:
-        return render(request, "data.html")
-    else:
-        return render(request, "input.html", {"error" : "Required all field"})
-
 data=[]
+def data_input(request):
+    return render(request, "input.html", {"values" : {}, "updateId": ""})
 
 def data_view(request):
-
     name = request.GET.get('name')
-    password = request.GET.get('pw')
-    # print("2",name, password)
-
+    password = request.GET.get('password')
+    id = request.GET.get('uid')
     if name and password:
-        data.append({"id":len(data)+1, "name":name, 'password': password})
-        print(data)
-
-    context={ "data":data }
-    # print(context)
+        if id and int(id) >= 0:
+            data[int(id)]['name'] = name
+            data[int(id)]['password'] = password
+        else:
+            data.append({"id":(int(len(data)-1))+1, "name":name, 'password': password})
+            print(data)
+    context={ "data" : data }
     return render(request, "data.html", context)
-                                                    
+                                                             
 def deletedata(request):
     id = request.GET.get('del')
     if id:
         global data
-        data = [x for x in data if str(x['id']) != str(id)]
+        data = [x for x in data if str(x['id']) !=  str(id)]
         return redirect(f"/get")
     else:
         return render(request,"data.html")
 
 def updatedata(request):
     id = request.GET.get('update')
-    print("id",id)
 
-    if id:
-        global data
-        data_on_id = lambda n : str(n['id']) == str(id)  
-        f_data = list(filter(data_on_id, data))
-        print("data",list(f_data))
-
-        context= {
-            'f_data' : f_data[0]
-            }
-        return render(request,"input.html", context)
-    else:
-        return HttpResponse("id is not found")
+    context= {
+        'values' : data[int(id)],
+        'updateId': int(id)
+    }
+    return render(request,"input.html", context)
     
+
+          
 
 
   
